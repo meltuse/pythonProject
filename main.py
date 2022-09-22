fdd8774fc49e8413433ad8c10c07e4162fac49bb
aruksans <alanruksans88@gmail.com>
2022-09-22 13:15:56 +0300

    do some tasks (2 tasks)

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    our_string = 'My name is somthing must go here'
+    result = our_string[0:10]
+    name = 'Alans'
+    print('{} {}'.format(result , name))
+    sliced_text = slice(10)
+    print('{} {}'.format(our_string[sliced_text], name))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
